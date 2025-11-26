fd30181e8774c8739f20cb273141dd265434f7f8
ramesh sah <rsah3798@gmail.com>
2025-11-26 16:13:16 +0545

    resize

--- rameshkumarsah.com.np/astro.config.mjs
+++ rameshkumarsah.com.np/astro.config.mjs
@@ -14,5 +14,6 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   site: 'https://rameshkumarsah.com.np',
+  mode: 'hybrid',
 
 });
---
+++ rameshkumarsah.com.np/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.rameshkumarsah.com.np',
+  appName: 'rameshkumarsah.com.np',
+  webDir: '.vercel/output/static/'
+};
+
+export default config;
